198da95793dec7de54f545ba40dcd515ed333de0
Simon Pero <simonraulpero@gmail.com>
2022-11-27 20:35:20 -0300

    jugador tiene la capacidad de decidir que hacer

--- js/script.js
+++ js/script.js
@@ -1,40 +1,28 @@
 alert("Bienvenido Usuario.")
+//variabels pregunta inicial
 let respuesta = ""
-let intentosPermitidos = 3
 
+//pregunta inicial para jugar
 do {
     respuestaIngresada = String(prompt("¿Quiere usted jugar un juego? Si o No")).toUpperCase()
     respuesta = respuestaIngresada
 } while (respuesta != "SI" && respuesta != "NO")
 
-
-turno = 0
+// variables del juego
+let turno = 0
 let answer = 0
 let bala = 1 + Math.floor(Math.random() * 6)
-let pierdeMaquina = 0
-let pierdeUsuario = 0
+let numero1 = 0
+let numero2 = 0
+let terminar = 0
+
+//usuario quiso jugar al juego
 if (respuesta == "SI") {
     alert("Estupendo, jugaremos a la ruleta rusa")
     alert("La bala ya ha sido colocada")
-    /* Sistema de turnos con For. Sin usar ya que creo que necesito IA para que sea funcional con un juego
-    for (let turno = 1; turno < 9; turno++) {
-        coma=false
-        answer=turno/2
-        console.log(answer)
-        if (parseInt(answer)!=answer) {
-            coma=true
-        }
-        console.log(coma)
-        
-        if (coma===true){
-            alert("es tu turno")
-            
-        } else {
-            alert("es mi turno")
-            
-        }   
-    }*/
+    //do while hasta que alguien pierda
     do {
+        //sistema de turnos que mira si answer tiene coma o no para decidir de quien es el turno
         turno++
         coma = false
         answer = turno / 2
@@ -48,31 +36,54 @@ if (respuesta == "SI") {
         if (coma === true) {
             alert("es tu turno")
             numero1 = 1 + Math.floor(Math.random() * 6)
-            pierdeUsuario = numero1
-            if (pierdeUsuario != bala) {
-                alert("click")
-                alert("parece que has tenido suerte")
-            }
+            //usuario tiene la opcion de dispararse a si mismo o la maquina
+            if (numero1 != bala) {
+                let disparo = ""
+                do {
+                    disparoIngresado = String(prompt("¿A quien disparas? Usuario/Maquina")).toUpperCase()
+                    disparo = disparoIngresado
+                } while (disparo != "USUARIO" && disparo != "MAQUINA")
 
+                if (disparo == "USUARIO") {
+                    alert("Click")
+                    alert("Parece que has tenido suerte")
+                } else {
+                    alert("Click")
+                    alert("Parece que no habia una bala")
+                }
+            }
+            if (numero1 === bala) {
+                do {
+                    disparoIngresado = String(prompt("¿A quien disparas? Usuario/Maquina")).toUpperCase()
+                    disparo = disparoIngresado
+                } while (disparo !== "USUARIO" && disparo !== "MAQUINA")
+
+                if (disparo == "USUARIO") {
+                    alert("BLAAM")
+                    alert("Parece que si habia una bala. Otro usuario te reemplazara")
+                    terminar = 1
+                } else if (disparo == "MAQUINA") {
+                    alert("BLAAM")
+                    alert("Enhorabuena, me has ganado, pero yo no desaparezco")
+                    terminar = 1
+                }
+            }
+            //turno de la maquina
         } else {
             alert("es mi turno")
             numero2 = 1 + Math.floor(Math.random() * 6)
-            pierdeMaquina = numero2
-            if (pierdeMaquina != bala) {
-                alert("click")
+            //maquina no decide si disparse a si misma o al usuario
+            if (numero2 != bala) {
+                alert("Click")
                 alert("parece que he tenido suerte")
+            } else if (numero2 === bala) {
+                alert("BLAAM")
+                alert("Enhorabuena, me has ganado, pero yo no desaparezco")
+                terminar = 1
             }
         }
-    } while (pierdeMaquina !== bala && pierdeUsuario !== bala)
+    } while (terminar !== 1)
+    //usuario no quizo jugar
 } else {
     alert("Es una pena")
-}
-
-
-if (pierdeMaquina === bala) {
-    alert("blaam")
-    alert("Enhorabuena, me has ganado")
-} else if (pierdeUsuario === bala) {
-    alert("blaam")
-    alert("Has perdido, otro usuario tomara tu lugar")
 }
